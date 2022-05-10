8c61fa6801075b565356bd4a66622740f41c6c30
AbbJesRib <54663096+AbbJesRib@users.noreply.github.com>
2022-05-10 13:56:20 +0200

    Fixed package error

--- verbalt-flode-app/src/components/RecordAudio.vue
+++ verbalt-flode-app/src/components/RecordAudio.vue
@@ -12,7 +12,6 @@ import Vue from "vue";
 import microm from "../main";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { S3Client } from "@aws-sdk/client-s3";
-import { createReadStream } from "node_modules/fs";
 import FormData from "form-data";
 
 export default Vue.extend({
@@ -63,25 +62,41 @@ export default Vue.extend({
       this.download();
       this.stream.getAudioTracks().forEach(track => track.stop());
     },
-    post: function() {
+    post: async function(mp3File) {
       const d = new Date();
       let time = d.getTime();
+      let newString = "";
+      for (let i = 1; i < time.toString().length; i += 2) {
+        newString += time.toString()[i];
+        newString += time.toString()[i-1];
+      }
+      const ID = Buffer.from(newString, "base64")
 
-      const client = new S3Client({ region: "us-west-2" });
+      const client = new S3Client({ region: "us-east-1" });
       const Bucket = "johnsmith";
       const Key = "user/eric/1";
       const Fields = {
         acl: "public-read",
       };
 
-      
+      var link = "https://2ug9xdpzd2.execute-api.us-east-1.amazonaws.com/tests/" + ID + "/upload"
+
+      const Conditions = [{ acl: "public-read" }, { bucket: "johnsmith" }, ["starts-with", "$key", "user/eric/"]];
+
+      const { url, fields } = await createPresignedPost(client, {
+        Bucket,
+        Key,
+        Conditions,
+        Fields,
+        Expires: 600, //Seconds before the presigned post expires. 3600 by default.
+      });
 
       const form = new FormData();
       Object.entries(Fields).forEach(([field, value]) => {
         form.append(field, value);
       });
-      form.append("file", createReadStream("path/to/a/file"));
-      form.submit("https://2ug9xdpzd2.execute-api.us-east-1.amazonaws.com/tests/", (err, res) => {
+      form.append("file", mp3File);
+      form.submit(, (err, res) => {
         //handle the response
       });
     }
